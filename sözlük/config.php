fdd99a8f24452f5362592e371b7017d3bd9dc201
korcilm <58364191+korcilm@users.noreply.github.com>
2020-08-14 17:48:07 +0300

    pagination işlemleri eklendi

--- sözlük/config.php
+++ sözlük/config.php
@@ -12,6 +12,25 @@ try {
 }catch (PDOException $error) {
 	echo "Database bağlantısı kurulamadı";
 	$error->getMessage();
-	
 }
+
+$results_per_page = 150;
+
+
+// veritabanındaki saklanan verinin sayısı
+$query=$db->query("SELECT * FROM kelime");
+$number_of_results=$query->rowCount();	
+
+
+// oluşacak toplam sayfa sayısını bulma
+$number_of_pages = ceil($number_of_results/$results_per_page);
+
+// ziyaretçinin o anda hangi sayfa numarasında olduğunu belirleme
+if (!isset($_GET['page'])) {
+	$page = 1;
+} else {
+	$page = $_GET['page'];
+}
+// görüntülenen sayfadaki sonuçlar için sql LIMIT başlangıç numarasını belirleme
+$this_page_first_result = ($page-1)*$results_per_page;
 ?>
